5d1eead856b521fbba7d3a81bf458d45dd99597a
heinrich van vught <heinrich.van.vught@gmail.com>
2016-06-20 19:02:23 +0100

    Up to level 4

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # angular_flatlander
 
+SUMMARY:
+==========================
+When writing an angular application, you should be able to write expressive application to show the intent of your application.
+
 MODULES:
 ==========================
 Where are application components live. You can have as many modules as you want, not only app.js
 
-DIRECTTIVES:
+CONTROLLERS:
+==========================
+Where behaviour lives. Where you define these (in your .js file) are actually the constructors of the controller!
+
+DIRECTIVES:
 ==========================
 Directives are HTML annotations that trigger Javascript behaviours.
 
@@ -18,6 +26,47 @@ ng-hide - hide an html object, if the expression is true
 
 ng-repeat - repeat the html section/object. uses special expression (LINQ-like) to define iteration
 
+ng-click - fires event scoped to the current scope
+
+ng-src - loads the src, after the browser is ready. Note syntaxt for ng-source is such that you surround it with '{{}}'
+
+ng-init - allows us to evaluate an expression within the current scope. 
+    It's use should be minimised as all logic should really go into controllers.
+    Although ng-init is great for prototyping, initialisation and configuration should go into controllers.
+
+ng-class - sets a class on the object if an expression is true. you can pass an object e.g. ng-class="{ active: tab === 1}"
+
+ng-model - binds the form element value to the property
+
+ng-submit - allows us to call a function when a form is submitted
+
+ng-include - passing a variable to a file to include. So passing a fixed string, you need to use "'file.html'"
+                - This could be a local file or a url. Angular will use Ajax to pull down the contents of that file.
+                - Loading into the page then happens.
+                - Using custom directives is preferable to directly including files.
+
+CUSTOM DIRECTIVES:
+==========================
+1. Dont use self-closing tags when using custom elements. Some browsers do not like this.!
+    // first custom directive, the directive use will be <product-title>
+    // hence the name in JS is 'productTitle' 
+    app.directive('productTitle', function()
+    {
+        //define the directive object
+        var definition = {};
+        definition.restrict = 'E';                         // type of directive (E for element)
+        definition.templateUrl = 'product-title.html';     // Url of a template
+        return definition;
+    });
+
+2. You will have issues if you are doign the tut directly from the file system for custom directives loading different files.
+ - see http://stackoverflow.com/questions/27742070/angularjs-error-cross-origin-requests-are-only-supported-for-protocol-schemes
+ - solution is to 
+        - npm install http-server -g
+        then do ...
+        - http-server C:\location\to\app.
+
+
 LEARNINGS / GOTCHAS:
 ==========================
 1. To get data from app.js / controller into HTML, you MUST attach data to the controller scope using "this.MyDataItem = dataItem" syntax
@@ -27,6 +76,13 @@ LEARNINGS / GOTCHAS:
 3. ng-click by default invokes methods scoped to the surrounding controller's scope!!!
 
 4. To keep your javascript sanity, i highly recommend using the 'MyController as controller' syntax and using scoped variables!
+
+5. When defining a property with a lambda for a new function, the lambda MUST end with a semi-colon (;)!!! 
+
+DATABINDINGS:
+Expressions such as {{tab}} define a two-way data binding. So that means the expression is re-evaluated when a property changes.
+-- The expression keeps an eye out for the value of tab so that when it does change, the expression is then updated to use this new value.
+
 
 DEBUG LEARNINGS:
 ==========================
--- index.js
+++ index.js
@@ -7,16 +7,76 @@
         {
             name: 'Dodecahedron',
             price: 2.95,
-            description: 'this is a fabulous gem',
+            description: 'This is a fabulous gem.',
             canPurchase: false,
-            soldOut: false
+            soldOut: false,
+            images: [
+                {
+                    full: 'imgs/ruby_large.jpg',
+                    thumb: 'imgs/ruby_sm.jpg'
+                },
+                {
+                    full: 'imgs/ruby_large1.jpg',
+                    thumb: 'imgs/ruby_sm1.jpg'
+                },
+                {
+                    full: 'imgs/ruby_large2.jpg',
+                    thumb: 'imgs/ruby_sm2.jpg'
+                },
+                {
+                    full: 'imgs/ruby_large3.jpg',
+                    thumb: 'imgs/ruby_sm3.jpg'
+                },
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this product!",
+                    author: "joe@thomas.com"
+                },
+                {
+                    stars: 1,
+                    body: "This product sucks!",
+                    author: "tim@hater.com"
+                }
+            ]
         },
         {
             name: 'Pentagonal Gem',
-            price: 2.95,
-            description: 'this is another gemmy thingie',
+            price: 2,
+            description: 'This is another gemmy thingie.',
             canPurchase: true,
-            soldOut: false
+            soldOut: false,
+            images: [
+                {
+                    full: 'imgs/ruby_large1.jpg',
+                    thumb: 'imgs/ruby_sm1.jpg'
+                },
+                {
+                    full: 'imgs/ruby_large1.jpg',
+                    thumb: 'imgs/ruby_sm1.jpg'
+                },
+                {
+                    full: 'imgs/ruby_large2.jpg',
+                    thumb: 'imgs/ruby_sm2.jpg'
+                },
+                {
+                    full: 'imgs/ruby_large3.jpg',
+                    thumb: 'imgs/ruby_sm3.jpg'
+                },
+            ],
+            reviews: [
+                {
+                    stars: 4,
+                    body: "I pretty much like!",
+                    author: "jenni@doester.com"
+                },
+                {
+                    stars: 2,
+                    body: "This product is how i say - not so goo.",
+                    author: "max@ontwosides.com"
+                }
+            ]
         },
     ]
 
@@ -24,23 +84,38 @@
         this.products = gems;
     });
 
-    /* -------------------- */
+    app.controller('PanelController', function () {
+        this.tab = 1;
 
-    /* COUNTER APPLICATION */
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
 
-    var countMin = 0;
-    var countMax = 5;
-
-    var countApp = angular.module("counterApp", []);
-    countApp.controller('counterController', function () {
-
-        this.numbers = [];
-        for (var i = countMin; i < countMax; ++i) {
-            this.numbers.push(i);
-            console.log(i);
-        }
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
     });
 
-    /* -------------------- */
+    app.controller('ReviewController', function() {
+        // create a brand new empty javascript object
+        this.review = {};
 
+        this.addReview = function(product)
+        {
+            this.review.createdOn = Date.now();
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+
+    // first custom directive, the directive use will be <product-title>
+    // hence the name in JS is 'productTitle' 
+    app.directive('productTitle', function()
+    {
+        //define the directive object
+        var definition = {};
+        definition.restrict = 'E';                         // type of directive (E for element)
+        definition.templateUrl = 'product-title.html';     // Url of a template
+        return definition;
+    });
 })();
